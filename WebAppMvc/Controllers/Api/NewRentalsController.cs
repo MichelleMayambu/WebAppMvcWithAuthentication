d14486067e1c2c52db4f095c92e5c3ad8d82685c
MichelleMayambu <your_email@example.com>
2020-01-30 16:52:46 +0200

    CreatedSimpleRentalApi

--- WebAppMvc/Controllers/Api/NewRentalsController.cs
+++ WebAppMvc/Controllers/Api/NewRentalsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,16 +7,45 @@ using System.Net.Http;
 using System.Web.Http;
 
 using WebAppMvc.Dtos;
+using WebAppMvc.Models;
 
 namespace WebAppMvc.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;//access database
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         [HttpPost]
         public IHttpActionResult NewRental(NewRentalDto newRental)
         {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
 
-            throw new NotImplementedException();
+            foreach ( var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+
+                };
+                _context.Rentals.Add(rental);
+            }
+            _context.SaveChanges();
+
+            return Ok();
+         
         }
     }
 }
